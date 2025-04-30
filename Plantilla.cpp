db59c7eabf746d0b50cc52a3bde00ea5d5db0df3
chanty25 <santiagohernaandez25@gmail.com>
2025-04-30 14:18:54 -0500

    Add files via upload

---
+++ Plantilla.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define ff first
+#define ss second
+#define endl '\n'
+#define all(s) s.begin(), s.end()
+#define sz(s) int(s.size())
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+typedef long long ll;
+typedef vector<int> vi;
+
+int main() {
+  
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    #endif
+    
+    return 0;
+}
